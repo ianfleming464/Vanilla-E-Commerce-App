d3c788c50cbf6d6c2422f7d81c46a5603b4cd6c3
ianfleming464 <ianflemingdeveloper@gmail.com>
2023-08-16 22:23:19 +0200

    Installed Tailwind, initialize function

--- index.js
+++ index.js
@@ -1,5 +1,61 @@
+import './styles.css';
+
 // Global variables
 
 const container = document.getElementById('container');
 const total = document.getElementById('total');
 let cart = [];
+
+// Function to append a product item to the container and update the total amount
+function appendProduct(item) {
+  const card = document.createElement('div');
+  card.classList.add('bg-white', 'p-4', 'rounded-lg', 'shadow-md', 'flex', 'flex-col', 'gap-2');
+
+  const id = item.id;
+
+  const title = document.createElement('h3');
+  title.textContent = item.title;
+  title.classList.add('text-lg', 'font-semibold', 'truncate');
+
+  const image = document.createElement('img');
+  image.src = item.image;
+  image.classList.add('w-full', 'rounded-md', 'object-scale-down', 'h-52', 'w-96', 'border-2');
+
+  const description = document.createElement('p');
+  description.textContent = item.description;
+  description.classList.add('text-sm', 'text-gray-600', 'text-justify');
+
+  const price = document.createElement('button');
+  price.textContent = formatPrice(item.price);
+  price.classList.add(
+    'bg-blue-100',
+    'text-black',
+    'rounded-md',
+    'px-2',
+    'py-1',
+    'text-sm',
+    'mt-auto',
+  );
+  price.addEventListener('click', () => {
+    totalAmount += item.price;
+    total.textContent = `Total: ${formatPrice(totalAmount)}`;
+    addToCart(item);
+  });
+
+  card.appendChild(title);
+  card.appendChild(image);
+  card.appendChild(description);
+  card.appendChild(price);
+
+  container.appendChild(card);
+}
+
+// Function to intialise the app
+
+function initialize() {
+  fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+    .then(products => products.forEach(item => console.log(item)));
+}
+
+initialize();

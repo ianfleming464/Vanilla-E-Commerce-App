c26eac66f20583755c01aa1028471059a232d36e
ianfleming464 <ianflemingdeveloper@gmail.com>
2023-08-25 00:22:36 +0200

    One render function as a single source of truth, whether load from an empty cart or local storage. Remove button working again

--- cart.js
+++ cart.js
@@ -19,16 +19,17 @@ export function addToCart(item) {
   const itemPrice = item.price;
   updateTotal(itemPrice);
 
-  // save the updated cart to local storage
+  // Save the updated cart to local storage
   saveCartToLocalStorage(cart);
 
-  // Update DOM with cart contents
-  updateCart();
+  // Render the updated cart
+  renderCart(cart);
   console.log(cart);
 }
 
 // Function to remove an item from the cart
 function removeFromCart(item) {
+  console.log('remove clicked', item.title);
   if (cart[item.id]) {
     const removedItem = cart[item.id];
 
@@ -39,14 +40,14 @@ function removeFromCart(item) {
     }
     const itemPrice = removedItem.price * removedItem.quantity;
 
-    // save updated cart to local storage
+    // Save the updated cart to local storage
     saveCartToLocalStorage(cart);
 
     // Update the total by subtracting the removed item's price
     updateTotal(-itemPrice);
 
-    // Update DOM with cart contents
-    updateCart();
+    // Render the updated cart
+    renderCart(cart);
   }
 
   // If the cart is empty, after items have been remove using the Remove button, keep the cart open - NOT WORKING
@@ -55,54 +56,6 @@ function removeFromCart(item) {
   //   cartElement.style.transform = 'translateX(0)';
   // }
 }
-
-// Function to append the cart contents to the DOM_object as cart
-function updateCart() {
-  const cartContent = document.querySelector('#cart .cart-content');
-  cartContent.innerHTML = '';
-
-  for (const itemId in cart) {
-    const item = cart[itemId];
-
-    // create a div for each item
-    const cartItem = document.createElement('div');
-    cartItem.classList.add('flex', 'flex-col', 'px-4', 'py-2', 'border-b');
-
-    // create a title for each item with a maximum of 4 words
-    const title = document.createElement('div');
-    title.textContent = item.title.split(' ').slice(0, 4).join(' '); // Limit to 4 words
-    title.classList.add(
-      'font-bold',
-      'text-lg',
-      'whitespace-nowrap',
-      'overflow-ellipsis',
-      'overflow-hidden',
-    );
-
-    // create a price for each item with a smaller font
-    const price = document.createElement('div');
-    price.textContent = formatPrice(item.price);
-    price.classList.add('text-sm');
-
-    // create a quantity field (initially blank)
-    const quantity = document.createElement('div');
-    quantity.textContent = `Quantity: ${item.quantity}`;
-
-    const removeButton = document.createElement('button');
-    removeButton.textContent = 'Remove';
-    removeButton.classList.add('bg-red-500', 'text-white', 'rounded-md', 'px-2', 'py-1', 'text-sm');
-    removeButton.addEventListener('click', () => removeFromCart(item));
-
-    // Add the elements to the cart item
-    cartItem.appendChild(title);
-    cartItem.appendChild(price);
-    cartItem.appendChild(quantity);
-    cartItem.appendChild(removeButton);
-
-    cartContent.appendChild(cartItem);
-  }
-}
-
 export function renderCart(cart) {
   const cartContent = document.querySelector('#cart .cart-content');
   cartContent.innerHTML = '';
--- initialize.js
+++ initialize.js
@@ -2,18 +2,26 @@ import { getProducts } from './api.js';
 import { appendProduct } from './product.js';
 import { loadCartFromLocalStorage } from './storage.js';
 import { renderCart } from './cart.js';
+import { updateTotal } from './total.js';
 
 export function initialize() {
   const container = document.getElementById('container');
   const cart = loadCartFromLocalStorage();
 
-  renderCart(cart);
+  let total = 0;
+  for (const itemId in cart) {
+    const item = cart[itemId];
+    total += item.price * item.quantity;
+  }
+
+  updateTotal(total);
 
   getProducts()
     .then(products => {
       products.forEach(item => {
         appendProduct(container, item);
       });
+      renderCart(cart);
       // alert('products fetched');
       // console.log('products fetched :', products);
     })
